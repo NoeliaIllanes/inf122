1ce61bbb2daba35260f73feba1ed414dd3f6e3a4
NoeliaIllanes <noeliaraquelillanesquilla@gmail.com>
2024-01-08 00:38:20 -0400

    5ENERO

---
+++ variables.js
@@ -0,0 +1,27 @@
+// Variables globales
+var nombre = "Noelia";
+console.log(nombre);
+var apellido = "Illanes";
+console.log(apellido);
+var edad = 19;
+console.log(edad);
+var sueldo = 4.234;
+console.log(sueldo);
+var casado = false;
+console.log(casado);
+// Variables de bloque
+let presupuesto = 1000;
+console.log(presupuesto);
+let gastos = 500;
+console.log(gastos);
+// Constantes
+const IVA = 13;
+console.log(IVA);
+const tipoCambio = 6.98;
+console.log(tipoCambio);
+var uni = "umsa";
+console.log(uni);
+let altura = 1.62;
+console.log(altura);
+const verdad = "E>L";
+console.log(verdad);
