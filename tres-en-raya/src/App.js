ec56f3e66ed517de4c5376bb4dc2f6584f2b7f33
NoeliaIllanes <146595917+NoeliaIllanes@users.noreply.github.com>
2024-01-17 13:27:48 +0000

    enero

--- tres-en-raya/src/App.js
+++ tres-en-raya/src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import Tablero from './components/Tablero';
 import TableroB from './components/TableroB';
 import TableroC from './components/TableroC';
+import TableroD from './components/TableroD';
+import TableroE from './components/TableroE';
 import Juego from './components/Juego';
-
+import JuegoA from './components/JuegoA';
 
 function App() {
+
   return (
     <div className="App">
       {/* <Tablero />
       <TableroB />
-      <TableroC /> */}
-      <Juego />
+      <TableroE />
+      <Juego />*/}
+      <JuegoA />
     </div>
   );
 }
+
 export default App;
