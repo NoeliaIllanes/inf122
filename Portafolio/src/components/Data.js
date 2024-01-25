a2739f30365d249e67f607e819a157373d622640
NoeliaIllanes <noeliaraquelillanesquilla@gmail.com>
2024-01-25 11:39:31 -0400

    2324enero

--- Portafolio/src/components/Data.js
+++ Portafolio/src/components/Data.js
@@ -7,8 +7,8 @@ function Data(){
         <section class="datos-cont">
         <p>3 <br></br> Semestres <br></br> de experiencia.</p>
         <p>1 <br></br> Project <br></br> Completed</p>
-        </section>
         <p>No hay clientes<br></br> satisfechos  <br></br>en este momento</p>
+        </section>
         </div>
     );
 }
--- Portafolio/src/components/Profile.js
+++ Portafolio/src/components/Profile.js
@@ -2,7 +2,6 @@ import React from "react";
 import "../style/Profile.css";
 import perfil from "../images/foto-perfil.jpg";
 import git from "../images/icons/github.svg";
-import yt from "../images/icons/youtube.svg";
 import X from "../images/icons/twitter.svg";
 import linke from "../images/icons/linkedin.svg";
 
@@ -18,7 +17,7 @@ function Profile() {
                 <p>Junior Front end</p>
                 <div className="icon-perfil">
                     <button onClick={hyperlink} ><img src={git}></img></button>
-                    <a href="https://github.com/NoeliaIllanes"><img  src={git}></img></a>
+                    {/* <a href="https://github.com/NoeliaIllanes"><img  src={git}></img></a> */}
                     <a href="https://twitter.com/Noehgtdrd4rzdf3"><img src={X}></img></a>
                     <a href="www.linkedin.com/in/noelia-illanes123"><img src={linke}></img></a>
                 </div>
