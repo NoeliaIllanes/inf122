7b0bdae6365507febcb416c463580817fabbdba0
NoeliaIllanes <noeliaraquelillanesquilla@gmail.com>
2024-01-25 11:38:48 -0400

    2324enero

---
+++ puppy-gallery/src/app/components/foto.js
@@ -0,0 +1,28 @@
+"use client"
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import style from "./foto.module.css";
+
+function Foto() {
+    const [perrito, setPerrito] = useState("/vercel.svg");
+    const [estado, setEstado] = useState("esperando");
+    const [abilities, setAbilities] = useState("habilidad");
+    const url = "https://pokeapi.co/api/v2/pokemon/eevee";
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setPerrito(data.sprites.front_default)
+                setEstado(data.species.name)
+                setAbilities(data.abilities[0].ability.name)
+            });
+    }, [])
+    return (
+        <div className={style.contenedor}>
+            <h1>{estado}</h1>
+            <h1>{abilities}</h1>
+            <Image src={perrito} alt="dog" width={500} height={500} />
+        </div>
+    );
+}
+export default Foto;
